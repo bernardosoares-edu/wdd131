d3c33b87b5df982792e9831d643ea8fb4da5d792
bernardosoares-edu <bsilvafilho@byupathway.edu>
2026-09-23 22:04:26 -0300

    atividades semana04

---
+++ semana04/scripts/carregamentolento.js
@@ -0,0 +1,5 @@
+// Ano atual
+document.getElementById("anoatual").textContent = new Date().getFullYear();
+ 
+// Última modificação
+document.getElementById("ultimaModificacao").textContent = `Última Modificação: ${document.lastModified}`;
